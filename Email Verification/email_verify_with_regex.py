34c2e157ac02f1e7c5af3ce30d08dd2b187e35c9
Muhammad Usman <usmanchaudhary112579@gmail.com>
2026-02-28 11:11:30 +0500

    Update email_verify_with_regex.py

--- Email Verification/email_verify_with_regex.py
+++ Email Verification/email_verify_with_regex.py
@@ -1,5 +1,5 @@
 import re
-email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+email_condition=r"^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
 user_email=input(' Enter your Email : ')
 
 if re.search(email_condition,user_email):
